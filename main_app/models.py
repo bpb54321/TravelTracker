53e24fbf1e68ca633fd961cc06e62f481e2b0f20
Brian Blosser <bpb54321@gmail.com>
2017-10-20 13:32:51 -0400

    Moved the Location model into models.py, view.py now referencing the Location model, and saved a few entries into the Locations table in the database.

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    predators = models.CharField(max_length=100)
+    num_restaurants = models.IntegerField()
+    img_url = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render
-from .models import models
+from .models import Location
 
 # Create your views here.
 
 
 def index(request):
-
+    locations = Location.objects.all()
     return render(request, 'index.html', {'locations': locations})
 
 
-class Location(models.Model):
-    name = models.CharField(max_length=100)
-    predators = models.CharField(max_length=100)
-    num_restaurants = models.IntegerField()
-    img_url = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
-locations = [
-    Location("Fool's Falls, CO", 'Flash Floods', 0,
-             'http://courseware.codeschool.com/try_django/images/fools-falls.jpg'),
-    Location("Curly's Creek, NM", 'Rattle Snakes', 2,
-             'http://courseware.codeschool.com/try_django/images/curlys-creek.jpg'),
-    Location('The Delicate Arch, UT', 'Scorpions', 0,
-             'http://courseware.codeschool.com/try_django/images/delicate-arch.jpg')
-]
